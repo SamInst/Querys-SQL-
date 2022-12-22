a392809d4d272d52d6054ec2da2491c15e09bd6e
SamInst <sam105666@ceuma.com.br>
2022-12-22 11:45:28 -0300

    consultas em sql armazenadas nas notas

---
+++ src/test/java/com/example/banco_teste/BancoTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banco_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
